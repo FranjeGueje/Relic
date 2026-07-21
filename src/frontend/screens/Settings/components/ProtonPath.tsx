60e114e6e4c36023ca7ff5f6c992764877e9f228
FranjeGueje <fjgj1@hotmail.com>
2026-07-21 04:05:06 +0200

    fix: tradu error, feat: detalles proton-umu

--- src/frontend/screens/Settings/components/ProtonPath.tsx
+++ src/frontend/screens/Settings/components/ProtonPath.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import useSetting from 'frontend/hooks/useSetting'
-import { PathSelectionBox } from 'frontend/components/UI'
+import { InfoBox, PathSelectionBox } from 'frontend/components/UI'
 import { hasHelp } from 'frontend/hooks/hasHelp'
 
 const ProtonPath = () => {
@@ -19,16 +19,26 @@ const ProtonPath = () => {
   const [protonPath, setProtonPath] = useSetting('protonPath', '')
 
   return (
-    <PathSelectionBox
-      type="directory"
-      onPathChange={setProtonPath}
-      path={protonPath}
-      pathDialogTitle={t('box.proton-path')}
-      pathDialogDefaultPath={protonPath}
-      label={t('setting.proton-path')}
-      htmlId="proton_path"
-      noDeleteButton
-    />
+    <>
+      <PathSelectionBox
+        type="directory"
+        onPathChange={setProtonPath}
+        path={protonPath}
+        pathDialogTitle={t('box.proton-path')}
+        pathDialogDefaultPath={protonPath}
+        label={t('setting.proton-path')}
+        htmlId="proton_path"
+        noDeleteButton
+      />
+      <InfoBox text={t('settings.advanced.details')}>
+        <p>
+          {t(
+            'setting.proton-path-info',
+            'Used to generate UMU prefixes and prepare Steam integrations.'
+          )}
+        </p>
+      </InfoBox>
+    </>
   )
 }
 
